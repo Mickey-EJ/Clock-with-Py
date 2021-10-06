cd1faead862918ab8cdbf5bb7c5408c57f4ab53a
Mickey <playmickey0601@gmail.com>
2021-10-06 18:26:16 +0530

    Added sample comments

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ root = Tk()
 root.title("Clock")
 
 def time():
+    #12 hour time format
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
 
+#clock face
 label = Label(root, font=("ds_digital", 80), background = "black", foreground = "cyan")
 label.pack(anchor = 'center')
 time()
